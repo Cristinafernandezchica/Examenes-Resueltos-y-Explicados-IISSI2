43e595bab4c66379f3e937965d407fdb46c4b896
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2024-06-12 18:27:33 +0200

    Examen Ordenar por Precio - Solución

---
+++ Simulacro-Examen-Ordenar-por-Precio/DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -0,0 +1,125 @@
+import { Restaurant, Product, RestaurantCategory, ProductCategory } from '../models/models.js'
+
+const index = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: { exclude: ['userId'] },
+        include:
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      },
+        order: [[{ model: RestaurantCategory, as: 'restaurantCategory' }, 'name', 'ASC']]
+      }
+    )
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const indexOwner = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: { exclude: ['userId'] },
+        where: { userId: req.user.id },
+        include: [{
+          model: RestaurantCategory,
+          as: 'restaurantCategory'
+        }]
+      })
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+// SOLUTION: Método para cambiar el estado de sortrByPrice según pulsemos el botón correspodiente
+const changeSort = async function (req, res) {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    restaurant.sortByPrice = !restaurant.sortByPrice
+    await restaurant.save() // hay que guardarlo si no, no se hace efectivo el cambio
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const create = async function (req, res) {
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = req.user.id // usuario actualmente autenticado
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+// SOLUTION
+const show = async function (req, res) {
+  // Only returns PUBLIC information of restaurants
+  try {
+    let restaurant = await Restaurant.findByPk(req.params.restaurantId) // Obtenemos el restaurante
+    // Variable que nos indicará la forma de ordenar los productos en función del atributo sortByPrice
+    const orderBy = restaurant.sortByPrice
+      ? [[{ model: Product, as: 'products' }, 'price', 'ASC']] // Por precio
+      : [[{ model: Product, as: 'products' }, 'order', 'ASC']] // Por defecto (order)
+    restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: orderBy // Utilizamos la constante creada
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const update = async function (req, res) {
+  try {
+    await Restaurant.update(req.body, { where: { id: req.params.restaurantId } })
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const destroy = async function (req, res) {
+  try {
+    const result = await Restaurant.destroy({ where: { id: req.params.restaurantId } })
+    let message = ''
+    if (result === 1) {
+      message = 'Sucessfuly deleted restaurant id.' + req.params.restaurantId
+    } else {
+      message = 'Could not delete restaurant.'
+    }
+    res.json(message)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const RestaurantController = {
+  index,
+  indexOwner,
+  create,
+  show,
+  update,
+  destroy,
+  changeSort //
+}
+export default RestaurantController
