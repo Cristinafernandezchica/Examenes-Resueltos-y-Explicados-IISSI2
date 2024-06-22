e8dd0c1cf392603947ee29fb5705c474b9c3e133
CRISTINA FERNANDEZ <correoparacristina100@gmail.com>
2024-06-22 18:31:06 +0200

    Ex Restaurantes Fijados - Solución + Anotaciones

---
+++ Examen-Restaurantes-Fijados/DeliverUS-Backend/src/controllers/RestaurantController.js
@@ -0,0 +1,136 @@
+import { Restaurant, Product, RestaurantCategory, ProductCategory, sequelizeSession } from '../models/models.js'
+
+const index = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: { exclude: ['userId'] },
+        include:
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      },
+        order: [[{ model: RestaurantCategory, as: 'restaurantCategory' }, 'name', 'ASC']]
+      }
+    )
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const indexOwner = async function (req, res) {
+  try {
+    const restaurants = await Restaurant.findAll(
+      {
+        attributes: { exclude: ['userId'] },
+        where: { userId: req.user.id },
+        // SOLUTION: Ordenamiento por fecha de fijado
+        order: [['pinned', 'DESC'], ['pinnedAt', 'ASC']],
+        include: [{
+          model: RestaurantCategory,
+          as: 'restaurantCategory'
+        }]
+      })
+    res.json(restaurants)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const create = async function (req, res) {
+  const newRestaurant = Restaurant.build(req.body)
+  newRestaurant.userId = req.user.id // usuario actualmente autenticado
+  try {
+    const restaurant = await newRestaurant.save()
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const show = async function (req, res) {
+  // Only returns PUBLIC information of restaurants
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId, {
+      attributes: { exclude: ['userId'] },
+      include: [{
+        model: Product,
+        as: 'products',
+        include: { model: ProductCategory, as: 'productCategory' }
+      },
+      {
+        model: RestaurantCategory,
+        as: 'restaurantCategory'
+      }],
+      order: [[{ model: Product, as: 'products' }, 'order', 'ASC']]
+    }
+    )
+    res.json(restaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const update = async function (req, res) {
+  try {
+    await Restaurant.update(req.body, { where: { id: req.params.restaurantId } })
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.json(updatedRestaurant)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+const destroy = async function (req, res) {
+  try {
+    const result = await Restaurant.destroy({ where: { id: req.params.restaurantId } })
+    let message = ''
+    if (result === 1) {
+      message = 'Sucessfuly deleted restaurant id.' + req.params.restaurantId
+    } else {
+      message = 'Could not delete restaurant.'
+    }
+    res.json(message)
+  } catch (err) {
+    res.status(500).send(err)
+  }
+}
+
+// SOLUTION
+const pin = async function (req, res) {
+  const trans = await sequelizeSession.transaction()
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    if (restaurant.pinned === true) {
+      await Restaurant.update(
+        { pinned: false, pinnedAt: null },
+        { where: { id: restaurant.id } },
+        { transaction: trans }
+      )
+    } else {
+      await Restaurant.update(
+        { pinned: true, pinnedAt: new Date() },
+        { where: { id: restaurant.id } },
+        { transaction: trans }
+      )
+    }
+    await trans.commit()
+    const updatedRestaurant = await Restaurant.findByPk(req.params.restaurantId)
+    res.json(updatedRestaurant)
+  } catch (error) {
+    await trans.rollback()
+    res.status(500).send(error)
+  }
+}
+
+const RestaurantController = {
+  index,
+  indexOwner,
+  create,
+  show,
+  update,
+  destroy,
+  pin // SOLUTION
+}
+export default RestaurantController
